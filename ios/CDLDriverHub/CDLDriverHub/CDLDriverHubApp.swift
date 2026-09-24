93b049f9bb211424b4098d0321258c81a88655ed
OlegD <116621563+OlegDik2007@users.noreply.github.com>
2026-09-24 15:19:48 -0500

    Migrate ios/CDLDriverHub/CDLDriverHub/CDLDriverHubApp.swift

---
+++ ios/CDLDriverHub/CDLDriverHub/CDLDriverHubApp.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+@main
+struct CDLDriverHubApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
